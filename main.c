312673205c7e5dc68b8d93042927357c5d73a9d3
SubduedCrane651 <95251533+SubdudedCrane651@users.noreply.github.com>
2022-05-22 15:13:21 +0000

    Modified to work in Linux (for some reason the char r[1024] gave a (null) on my Ubuntu machine solved it with malloc

--- main.c
+++ main.c
@@ -26,57 +26,78 @@ void sigint_handler(int sig_num)
 
 char *Security(char *d)
 {
-    char r[1024];;
-    char cmd[1024];
-        snprintf(cmd, sizeof(cmd), "awk '/Failed/ && /%s/ {print $1, $2, $3,$6,$9,$11,$12,$13}' auth.log", d);
-        printf("%s\n",cmd);
+    char *r=NULL;
+    r = malloc(125);
+    //char r[1024]= "\0";
+    char cmd[125]= "\0";
+    snprintf(cmd, sizeof(cmd), "awk '/Failed/ && /%s/ {print $1, $2, $3,$6,$9,$11,$12,$13}' auth.log", d);
+    //printf("%s\n",cmd);
     FILE *fp;
     int status;
+    sleep(1);
     if ((fp = popen(cmd,"r")) == NULL)
-        {} else
+    {}
+    else
         //return "none";
 
-    // print_all(fp);
-    {int c;
-    while ((c = getc(fp)) != EOF)
-        strncat(r, &c, 1);}
+        // print_all(fp);
+        snprintf(r,sizeof(r),"%s","");
+    {
+        int c;
+        while ((c = getc(fp)) != EOF)
+            strncat(r, &c, 1);
+    }
     pclose(fp);
     return r;
 }
 
-char *Month(int choice) {
+char *Month(int choice)
+{
 
-  char *str;
-   switch(choice)
+    char *str;
+    switch(choice)
     {
-        case 1: str="Jan";
-                break;
-        case 2: str="Feb";
-                break;
-        case 3: str="Mar";
-                break;
-        case 4: str="Apr";
-                break;
-        case 5: str="May";
-                break;
-        case 6: str="Jun";
-                break;
-        case 7: str="Jul";
-                break;
-        case 8: str="Aug";
-                break;
-        case 9: str="Sep";
-                break;
-        case 10: str="Oct";
-                break;
-        case 11: str="Nov";
-                break;
-        case 12: str="Dec";
-                break;
-        default : printf("invalid number");
+    case 1:
+        str="Jan";
+        break;
+    case 2:
+        str="Feb";
+        break;
+    case 3:
+        str="Mar";
+        break;
+    case 4:
+        str="Apr";
+        break;
+    case 5:
+        str="May";
+        break;
+    case 6:
+        str="Jun";
+        break;
+    case 7:
+        str="Jul";
+        break;
+    case 8:
+        str="Aug";
+        break;
+    case 9:
+        str="Sep";
+        break;
+    case 10:
+        str="Oct";
+        break;
+    case 11:
+        str="Nov";
+        break;
+    case 12:
+        str="Dec";
+        break;
+    default :
+        printf("invalid number");
     }
 
-   return str; 
+    return str;
 }
 
 int main ()
@@ -85,22 +106,27 @@ int main ()
 
     /* Infinite loop */
     while(1)
-    {       
+    {
         time_t t = time(NULL);
-  struct tm tm = *localtime(&t);
-  // printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-      
+        struct tm tm = *localtime(&t);
+        // printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
 // printf("Date and time: %s %d %02d:%02d:%02d\n",Month(tm.tm_mon+1),tm.tm_mday,tm.tm_hour, tm.tm_min, tm.tm_sec);
-      // snprintf(d, sizeof(d), "%s %d %d:%d:%d", Month(tm.tm_mon+1), tm.tm_mday,tm.tm_hour, tm.tm_min, tm.tm_sec);
-      // d=Month(tm.tm_mon+1);
-      char d[1024];
         // snprintf(d, sizeof(d), "%s %d %d:%d:%d", Month(tm.tm_mon+1), tm.tm_mday,tm.tm_hour, tm.tm_min, tm.tm_sec);
-      snprintf(d,sizeof(d),"%s","May 20 17:41:02");
-      // char *sec=Security(d);
-      char sec[1024];
-      strncpy( sec, Security(d), sizeof( sec ) );
-printf("%s\n",sec);
-      printf("%s\n",d);
+        // d=Month(tm.tm_mon+1);
+        char d[1024];
+        // snprintf(d, sizeof(d), "%s %d %d:%d:%d", Month(tm.tm_mon+1), tm.tm_mday,tm.tm_hour, tm.tm_min, tm.tm_sec);
+        snprintf(d,sizeof(d),"%s","May 20 17:41:02");
+        // char *sec=Security(d);
+        // strncpy( sec, Security(d), sizeof( sec ) );
+        char sec[125]= "\0";
+        snprintf(sec,sizeof(sec),"%s",Security(d));
+
+        if (strlen(sec)!=0)
+        {
+            printf("%s\n",d);
+            printf("%s\n",sec);
+        }
     }
     return 0;
 }
